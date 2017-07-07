18064f0d28a92950d37cdc38bf7fe3daab79f00a
Norbert Auer on hoth <norbert@derauer.net>
2017-07-07 09:17:11 +0200

    Add NCBIProteinFastaLoaderTask | Update build.xml

---
+++ bio/sources/fasta/main/src/org/intermine/bio/dataconversion/NCBIProteinFastaLoaderTask.java
@@ -0,0 +1,142 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2017 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+
+import org.biojava.bio.seq.Sequence;
+import org.intermine.model.bio.BioEntity;
+import org.intermine.model.bio.DataSet;
+import org.intermine.model.bio.Organism;
+import org.intermine.objectstore.ObjectStoreException;
+
+/**
+ * A loader that works for FASTA files with an NCBI formatted header:
+ * http://www.ncbi.nlm.nih.gov/blast/fasta.shtml
+ * http://en.wikipedia.org/wiki/Fasta_format
+ * @author Kim Rutherford
+ */
+//public class NCBIProteinFastaLoaderTask extends FastaLoaderTask
+//{
+//    //protected static final Logger LOG = Logger.getLogger(NCBIFastaLoaderTask.class);
+//    private static final String ORG_HEADER = " Homo sapiens ";
+//    private static final String CHROMOSOME_HEADER = "chromosome";
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    protected String getIdentifier(Sequence bioJavaSequence) {
+//        Annotation anno = bioJavaSequence.getAnnotation();
+//        String header = anno.getProperty("description_line").toString();
+//        String desc = bioJavaSequence.getAnnotation().getProperty("description").toString();
+//        // >gi|568815597|ref|NC_000001.11| Homo sapiens chromosome 1, GRCh38.p2 Primary Assembly
+//        // gi|251831106|ref|NC_012920.1| Homo sapiens mitochondrion, complete genome
+//        for (String headerString : header.split("\\|")) {
+//            if (headerString.contains("mitochondrion")) {
+//                return "MT";
+//            }
+//            // we want the phrase with "chromosome" in it
+//            if (headerString.contains(CHROMOSOME_HEADER)) {
+//                // chop off the part after the comma
+//                String[] headerSubStrings = headerString.split(",");
+//                // chop off everything but the chromosome number
+//                String identifier = headerSubStrings[0].substring(ORG_HEADER.length()
+//                        + CHROMOSOME_HEADER.length());
+//                return identifier.trim();
+//
+//            }
+//        }
+//        // nothing found
+//        throw new RuntimeException("Couldn't find chromosome identifier " + header);
+//    }
+//}
+
+public class NCBIProteinFastaLoaderTask extends FastaLoaderTask
+{
+	
+	 /**
+	  * For the given BioJava Sequence object, return the description to be used when creating
+	  * the corresponding BioEntity.
+	  * i.e. NADH dehydrogenase subunit 5 (mitochondrion) [Cricetulus griseus]
+	  * @param bioJavaSequence the sequenece
+	  * @return an identifier
+	  */	  
+	  protected String getDescription(Sequence bioJavaSequence)
+	  {          
+	      String desc = bioJavaSequence.getAnnotation().getProperty("description").toString();
+	
+	      return desc;
+	  }
+
+      
+      /**
+       * For the given BioJava Sequence object, return the protein RefSeq name to be used when creating
+       * the corresponding BioEntity.
+       * if | is present the middle bit is returned, eg gi|91176213|ref|YP_537129.1| NADH dehydrogenase subunit 5 (mitochondrion) [Cricetulus griseus]
+       * @param bioJavaSequence the sequenece
+       * @return an identifier
+       */	  
+      protected String getProteinName(Sequence bioJavaSequence)
+      {
+          String name = bioJavaSequence.getName();
+
+          if (name.contains("|"))
+          {
+              String[] bits = name.split("\\|");
+              if (bits.length < 2) {
+                  return null;
+              }
+              name = bits[bits.length - 1];
+          }
+          else
+            return null;
+
+          return name;
+      }
+      
+      
+    /**
+     * Do any extra processing needed for this record (extra attributes, objects, references etc.)
+     * This method is called before the new objects are stored
+     * @param bioJavaSequence the BioJava Sequence
+     * @param sequence the Sequence
+     * @param bioEntity the object that references the Sequence
+     * @param organism the Organism object for the new InterMineObject
+     * @param dataSet the DataSet object
+     * @throws ObjectStoreException if a store() fails during processing
+     */
+    @Override
+    protected void extraProcessing(Sequence bioJavaSequence,
+            org.intermine.model.bio.Sequence sequence,
+            BioEntity bioEntity, Organism organism, DataSet dataSet)
+        throws ObjectStoreException
+        {
+          // String header = bioJavaSequence.getName();
+          String desc = getDescription(bioJavaSequence);
+          
+          int StartOrganism = desc.indexOf("[");
+          
+          // Remove optional organism part i.e. [Cricetulus griseus]
+          if (StartOrganism > 0)
+        	  desc = desc.substring(0, StartOrganism).trim();
+
+          String refseqId = getProteinName(bioJavaSequence);
+
+          if (refseqId != null)
+          {
+              bioEntity.setFieldValue("refseqAccession", refseqId);
+              bioEntity.setFieldValue("name", desc);
+          }
+        }
+    
+    
+}
+
---
+++ bio/sources/fasta/test/src/org/intermine/bio/dataconversion/NCBIProteinFastaLoaderTaskTest.java
@@ -0,0 +1,145 @@
+package org.intermine.bio.dataconversion;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
+
+import org.intermine.metadata.ConstraintOp;
+import org.intermine.model.InterMineObject;
+import org.intermine.model.bio.DataSet;
+import org.intermine.model.bio.Protein;
+import org.intermine.model.bio.Sequence;
+import org.intermine.objectstore.ObjectStoreWriterFactory;
+import org.intermine.objectstore.query.ContainsConstraint;
+import org.intermine.objectstore.query.Query;
+import org.intermine.objectstore.query.QueryClass;
+import org.intermine.objectstore.query.QueryObjectReference;
+import org.intermine.objectstore.query.Results;
+import org.intermine.objectstore.query.SingletonResults;
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.intermine.objectstore.ObjectStore;
+import org.intermine.objectstore.ObjectStoreWriter;
+
+public class NCBIProteinFastaLoaderTaskTest extends TestCase {
+
+	private ObjectStoreWriter osw;
+    private static final Logger LOG = Logger.getLogger(NCBIFastaLoaderTaskTest.class);
+    private String dataSetTitle = "ncbi test title";
+    private final String dataSourceName = "test-source";
+    
+	@Override
+    public void setUp() throws Exception {
+        osw = ObjectStoreWriterFactory.getObjectStoreWriter("osw.bio-test");
+        osw.getObjectStore().flushObjectById();
+    }
+	
+	@Override
+    public void tearDown() throws Exception {
+        LOG.info("in tear down");
+        if (osw.isInTransaction()) {
+            osw.abortTransaction();
+        }
+        Query q = new Query();
+        QueryClass qc = new QueryClass(InterMineObject.class);
+        q.addFrom(qc);
+        q.addToSelect(qc);
+        SingletonResults res = osw.getObjectStore().executeSingleton(q);
+        LOG.info("created results");
+        Iterator<Object> resIter = res.iterator();
+        osw.beginTransaction();
+        while (resIter.hasNext()) {
+            InterMineObject o = (InterMineObject) resIter.next();
+            System.out.println("deleting: " + o.getId());
+            osw.delete(o);
+        }
+        osw.commitTransaction();
+        LOG.info("committed transaction");
+        osw.close();
+        LOG.info("closed objectstore");
+    }
+	
+//	public void testExtraProcessing() {
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testGetDescriptionSequence() {
+//		NCBIProteinFastaLoaderTask
+//		fail("Not yet implemented");
+//	}
+//
+//	public void testGetProteinName() {
+//		fail("Not yet implemented");
+//	}
+	
+	public void testFastaLoad() throws Exception {
+		NCBIProteinFastaLoaderTask flt = new NCBIProteinFastaLoaderTask();
+        flt.setFastaTaxonId("10029");
+        flt.setIgnoreDuplicates(true);
+        flt.setSequenceType("protein");
+        flt.setClassName("org.intermine.model.bio.Protein");
+        flt.setIntegrationWriterAlias("integration.bio-test");
+        flt.setSourceName("fasta-test");
+        flt.setDataSetTitle(dataSetTitle);
+        flt.setDataSourceName(dataSourceName);
+        flt.setClassAttribute("primaryAccession");
+
+        File[] files = new File[1];
+        files[0] = File.createTempFile("NCBIProteinFastaLoaderTaskTest", "tmp");
+        FileWriter fw = new FileWriter(files[0]);
+        InputStream is =
+            getClass().getClassLoader().getResourceAsStream("NCBIProtein.fasta");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            fw.write(line + "\n");
+        }
+
+        fw.close();
+        files[0].deleteOnExit();
+        flt.setFileArray(files);
+        flt.execute();
+
+        //Check the results to see if we have some data...
+        ObjectStore os = osw.getObjectStore();
+
+        Query q = new Query();
+        QueryClass queryClass = new QueryClass(Protein.class);
+        QueryClass seqQueryClass = new QueryClass(Sequence.class);
+        q.addToSelect(queryClass);
+        q.addToSelect(seqQueryClass);
+        q.addFrom(queryClass);
+        q.addFrom(seqQueryClass);
+
+        QueryObjectReference qor = new QueryObjectReference(queryClass, "sequence");
+        ContainsConstraint cc = new ContainsConstraint(qor, ConstraintOp.CONTAINS, seqQueryClass);
+
+        q.setConstraint(cc);
+
+        Results r = os.execute(q);
+
+        assertEquals(2, r.size());
+
+        Protein protein = (Protein) ((List) r.get(0)).get(0);
+
+        assertEquals("91176210", protein.getPrimaryAccession());
+        assertEquals("10029", protein.getOrganism().getTaxonId());
+
+        DataSet dataSet = protein.getDataSets().iterator().next();
+        assertEquals(dataSetTitle, dataSet.getName());
+        assertEquals(dataSourceName, dataSet.getDataSource().getName());
+
+        /*
+        >gi|91176210|ref|YP_537126.1| NADH dehydrogenase subunit 3 (mitochondrion) [Cricetulus griseus]
+        */
+        assertEquals("MNLIMAISTNIILSLVLISVAFWLPQLNTYTEKAGPYECGFDPMSSARLPFSMKFFLVAI"
+        			 + "TFLLFDLEIALLLPLPWAMQSMTINMMLTISFLFLSILGLGLAYEWKQKGLEWTE", protein.getSequence().getResidues().toString());
+	}
+
+}
